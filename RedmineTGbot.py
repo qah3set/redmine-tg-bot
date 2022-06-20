e98838f26db6e410c64a8646e5b40b635cb5e431
TemKaa1337 <temkaaw0wpro@gmail.com>
2022-06-20 16:30:08 +0300

    reformat code

--- RedmineTGbot.py
+++ RedmineTGbot.py
@@ -2,99 +2,88 @@ from telegram import Update, ParseMode
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from redminelib import Redmine
 
-token = "<token>"
-redmine_url = "<url>"
+TOKEN = "<token>"
+REDMINE_URL = "<url>"
+
 
 def start(update: Update, context: CallbackContext) -> None:
-
-    chat_id = update.message.chat_id
-
-    if "api" in context.user_data:
-
-        remove_job_if_exists(str(chat_id), context)
-
-        context.job_queue.run_repeating(
-            alarm, 10, 1, context=context, name=str(chat_id))
-        update.message.reply_text('Бот запущен!')
-    else:
-
+    chat_id = str(update.message.chat_id)
+    if "api" not in context.user_data:
         update.message.reply_text(
             'Привет! Используйте команду /api <свой ключ api> чтобы запустить бота')
+        return
+
+    remove_jobs_if_exist(chat_id, context)
+    context.job_queue.run_repeating(
+        alarm, 10, 1, context=context, name=chat_id)
+    update.message.reply_text('Бот запущен!')
 
 
 def alarm(context: CallbackContext) -> None:
-
     user_data = context.job.context.user_data
-    api = user_data["api"]
-    chat_id = user_data["chat_id"]
-    issues_new = []
-    issues_old = []
-    if "issues_old" in user_data:
-        issues_old = user_data["issues_old"]
+    new_issues_id = []
+    old_issues_id = user_data["issues_old"] if "issues_old" in user_data else []
 
-    redmine = Redmine(redmine_url, key=api)
+    redmine = Redmine(REDMINE_URL, key=user_data["api"])
     issues = redmine.issue.filter(status_id="open", assigned_to_id="me")
 
     for issue in issues:
-        issues_new.append(issue.id)
+        new_issues_id.append(issue.id)
 
-        if issue.id not in issues_old:
+        if issue.id not in old_issues_id:
             txt = "Номер задачи: <a href='{0}/issues/{1}'>{1}</a>\nТема задачи: {2}\nНазначена: {3}\nТрекер: {4}\nСтатус: {5}\nПриоритет: {6}".format(
-                redmine_url, issue.id, issue.subject, issue.assigned_to, issue.tracker, issue.status, issue.priority)
+                REDMINE_URL, issue.id, issue.subject, issue.assigned_to, issue.tracker, issue.status, issue.priority)
             context.bot.send_message(
-                chat_id, text=txt, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+                user_data["chat_id"], text=txt, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
 
-    issues_old = issues_new.copy()
-
-    user_data["issues_old"] = issues_old
+    user_data["issues_old"] = new_issues_id.copy()
 
 
-def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
-    current_jobs = context.job_queue.get_jobs_by_name(name)
-    if not current_jobs:
-        return False
-    for job in current_jobs:
-        job.schedule_removal()
-    return True
+def jobs_exist(name: str, context: CallbackContext) -> bool:
+    return bool(context.job_queue.get_jobs_by_name(name))
+
+
+def remove_jobs_if_exist(name: str, context: CallbackContext) -> None:
+    if jobs_exist(name, CallbackContext):
+        current_jobs = context.job_queue.get_jobs_by_name(name)
+        for job in current_jobs:
+            job.schedule_removal()
 
 
 def api(update: Update, context: CallbackContext) -> None:
-    chat_id = update.message.chat_id
-    api = str(context.args[0])
+    chat_id = str(update.message.chat_id)
+    api_key = str(context.args[0])
 
-    if len(api) != 40:
+    if len(api_key) != 40:
         update.message.reply_text(
             'Введен некорректный ключ API, используйте команду: /api <ключ api>')
         return
 
-    context.user_data["api"] = api
-    context.user_data["chat_id"] = chat_id
-    remove_job_if_exists(str(chat_id), context)
+    context.user_data["api"] = api_key
+    context.user_data["chat_id"] = update.message.chat_id
+    remove_jobs_if_exist(chat_id, context)
     context.job_queue.run_repeating(
-        alarm, 10, 1, context=context, name=str(chat_id))
+        alarm, 10, 1, context=context, name=chat_id)
     update.message.reply_text('Бот запущен!')
 
 
 def stop(update: Update, context: CallbackContext) -> None:
-    chat_id = update.message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    text = 'Обновление задач отменено' if job_removed else 'У Вас нет активных обновлений задач'
+    chat_id = str(update.message.chat_id)
+    jobs_removed = jobs_exist(chat_id, context)
+    remove_jobs_if_exist(chat_id, context)
 
-    update.message.reply_text(text)
+    update.message.reply_text('Обновление задач отменено' if jobs_removed else 'У Вас нет активных обновлений задач')
 
 
 def main() -> None:
+    updater = Updater(TOKEN)
 
-    updater = Updater(token)
-    dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("api", api))
-    dispatcher.add_handler(CommandHandler("help", start))
-    dispatcher.add_handler(CommandHandler("stop", stop))
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+    updater.dispatcher.add_handler(CommandHandler("api", api))
+    updater.dispatcher.add_handler(CommandHandler("help", start))
+    updater.dispatcher.add_handler(CommandHandler("stop", stop))
 
     updater.start_polling()
-
     updater.idle()
 
 
